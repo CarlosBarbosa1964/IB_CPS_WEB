bf33266cce54aa018babc265601b0df971be27c8
rparana <paranafael@yahoo.com.br>
2015-07-07 10:04:08 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/ibtecnologia/App.java
@@ -0,0 +1,13 @@
+package br.com.ibtecnologia;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
